6c711dcb4f00204dd86912f8ad44d5ee7f97e2dc
mcskumy <romain.turchet@mediacrossing.fr>
2013-11-06 14:33:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="fr-FR">
+<head>
+<title>//mediacrossing:</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="robots" content="noindex,nofollow" />
+
+<style>
+   body{
+    margin: 0;
+    padding: 50px 0 0;
+    background: #1e1e1e;
+    font-family: Arial;
+    font-size: 160%;
+    text-align: center;
+    color: #fff;
+   }
+   
+   img{
+    border: 0;
+   }
+   
+   a{
+    border: 0;
+   }
+
+</style>
+
+</head>
+
+<body>
+	
+	<img src="images/logo-mediacrossing.png" alt="Mediacrossing" />
+	
+	<p>//mediacrossing: is here !</p>
+		
+</body>
+
+</html>
